ae028fcee7ff6ffe959ebe4324b9b865c4cddccf
kenbme <guilhermekenned1@gmail.com>
2023-02-22 17:31:50 -0300

    feat: latex support and global css

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,14 +3,17 @@ import Link from "next/link";
 
 export default function Post({ post }) {
     return (
-        <div style={{maxWidth: "1200px", margin: "0 auto", padding: "10px"}}>
-        <Link href={"/"}>Voltar</Link>
-        <div>
-            <h1>{post.data.title}</h1>
-            <h2>{post.data.discipline}</h2>
-            <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
-        </div>
-        </div>
+        <>
+            <div style={{ maxWidth: "1200px", margin: "0 auto", padding: "10px" }}>
+                <Link href={"/"}>Voltar</Link>
+                <div>
+                    <h1>{post.data.title}</h1>
+                    <h2>{post.data.discipline}</h2>
+                    <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
+                </div>
+            </div>
+            <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+        </>
     )
 }
 
