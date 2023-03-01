30b9ef5b9717e54905dd9a22ef87aecc22895cb7
kenbme <guilhermekenned1@gmail.com>
2023-03-01 19:08:14 -0300

    feat: Post Contribution Link

--- components/PostSingle.jsx
+++ components/PostSingle.jsx
@@ -1,4 +1,7 @@
 import styles from "@/styles/PostSingle.module.css"
+import Link from "next/link"
+import Image from 'next/image';
+import { useRouter } from "next/router";
 
 export default function Page({ post }) {
     return (
@@ -7,6 +10,12 @@ export default function Page({ post }) {
                 <h1>{post.data.title}</h1>
                 <h2>{post.data.discipline}</h2>
                 <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
+            </div>
+            <div className={styles.Contribute}>
+                <Link href={`https://github.com/kenbme/markai/blob/main/posts/${post.id}.md`}>
+                    <p>Quer contribuir com esta página?</p>
+                    <Image src="/assets/svg/github.svg" width="32" height="32" alt="GitHub" />
+                </Link>
             </div>
         </section>
     )
