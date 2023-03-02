7f3d62a470df57247201a828e719799564be341f
kenbme <guilhermekenned1@gmail.com>
2023-03-02 07:19:26 -0300

    fix: removing next/image

--- components/DIsciplines.jsx
+++ components/DIsciplines.jsx
@@ -1,6 +1,5 @@
 import styles from "@/styles/Disciplines.module.css";
 import Link from "next/link";
-import Image from "next/image";
 
 export default function Disciplines({ disciplines }) {
     return (
@@ -10,7 +9,7 @@ export default function Disciplines({ disciplines }) {
                     {disciplines.map((discipline) =>
                         <li key={discipline.name}>
                             <Link href={`disciplinas/${discipline.name}`}>
-                                <Image src={discipline.photo} width="300" height="300" alt="Imagem da disciplina"/>
+                                <img src={discipline.photo} alt="Imagem da disciplina" />
                                 <div>
                                     {discipline.name}
                                 </div>
--- next.config.js
+++ next.config.js
@@ -1,27 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
-
-    // If client-side, don't polyfill `fs`
     if (!isServer) {
       config.resolve.fallback = {
         fs: false,
       };
     }
-
     return config;
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-        port: '',
-        pathname: '**',
-      },
-    ],
   }
 }
-
-module.exports = nextConfig
