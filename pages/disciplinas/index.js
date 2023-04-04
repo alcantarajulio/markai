946f4327efe59eb95bb4d01a42d79130e6c0d3b4
kenbme <guilhermekenned1@gmail.com>
2023-04-04 01:35:21 -0300

    feat: filtro de disciplimas e posts (experimental)

--- pages/disciplinas/index.js
+++ pages/disciplinas/index.js
@@ -1,21 +1,68 @@
 import Disciplines from "@/components/Disciplines";
 import { getAllDisciplines } from "@/services/discipline.service"
-import React from "react";
+import React, { useState } from "react";
 
 /**
- * @param {{disciplines: import("../../services/discipline.service").Discipline[]}} props
+ * @param {{disciplines: import("../../services/discipline.service").Discipline[], periods: number[]}} props
  */
-export default function Page({ disciplines }) {
+export default function Page({ disciplines, periods }) {
+  const [disciplinesFiltered, setDisciplinesFiltered] = useState(disciplines);
+
+  /**
+   * 
+   * @param {React.ChangeEvent<HTMLSelectElement>} e 
+   */
+  function filterDisciplineByPeriod(e) {
+    if (e.target.value == "") {
+      setDisciplinesFiltered(disciplines);
+      return;
+    }
+    const period = parseInt(e.target.value);
+    /**
+     * @type {import("@/services/discipline.service").Discipline[]}
+     */
+    const _disciplinesFiltered = [];
+    disciplines.forEach(discipline => {
+      if (discipline.period == period) {
+        _disciplinesFiltered.push(discipline);
+      }
+    });
+    setDisciplinesFiltered(_disciplinesFiltered);
+  }
+
+  const options = () => {
+    return (
+      <>
+        {(() => {
+          let rows = [<option value={""}></option>];
+          periods.forEach(i => {
+            rows.push(<option value={i}>{i}º</option>);
+          });
+          return rows;
+        })()}
+      </>
+    );
+  }
+
   return (
     <>
-      <Disciplines disciplines={disciplines} />
+      <div style={{ padding: "0px 16px", margin: 0, marginTop: "16px" }}>
+        <select style={{ padding: "8px", border: "2px solid black" }} onChange={(e) => filterDisciplineByPeriod(e)}>
+          {options()}
+        </select>
+      </div>
+      <Disciplines disciplines={disciplinesFiltered} />
     </>
   )
 }
 
 export async function getStaticProps() {
   const disciplines = getAllDisciplines();
+  const periods = new Set();
+  disciplines.forEach((discipline) => {
+    periods.add(discipline.period);
+  });
   return {
-    props: { disciplines: disciplines }
+    props: { disciplines: disciplines, periods: Array.from(periods) }
   }
 }
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,18 +1,60 @@
 import Posts from "@/components/Posts";
 import { getAllPosts } from "@/services/post.service"
-import React from "react";
+import React, { useState } from "react";
 
 /**
  * @typedef {{id: string; title: string; discipline: string; abr: string;}} Post
  */
 
 /**
- * @param {{posts: Post[]}} props
+ * @param {{posts: Post[], disciplinesAbr: Set<string>}} props
  */
-export default function Page({ posts }) {
+export default function Page({ posts, disciplinesAbr }) {
+  const [postsFiltered, setPostsFiltered] = useState(posts);
+
+  /**
+   * @param {React.ChangeEvent<HTMLSelectElement>} e
+   */
+  function filterPostsByDiscipline(e) {
+    if (e.target.value == "") {
+      setPostsFiltered(posts);
+      return;
+    }
+    const abr = e.target.value;
+    /**
+     * @type {Post[]}
+     */
+    const _postsFiltered = [];
+    posts.forEach(post => {
+      if (post.abr == abr) {
+        _postsFiltered.push(post);
+      }
+    });
+    setPostsFiltered(_postsFiltered);
+  }
+
+  const options = () => {
+    return (
+      <>
+        {(() => {
+          let rows = [<option value={""}></option>];
+          disciplinesAbr.forEach(abr => {
+            rows.push(<option value={abr}>{abr}</option>);
+          });
+          return rows;
+        })()}
+      </>
+    );
+  }
+
   return (
     <>
-      <Posts posts={posts} />
+      <div style={{ padding: "0px 16px", margin: 0, marginTop: "16px" }}>
+        <select style={{ padding: "8px", border: "2px solid black" }} onChange={(e) => filterPostsByDiscipline(e)}>
+          {options()}
+        </select>
+      </div>
+      <Posts posts={postsFiltered} />
     </>
   )
 }
@@ -20,14 +62,16 @@ export default function Page({ posts }) {
 export async function getStaticProps() {
   /** @type {Post[]} */
   const posts = [];
+  let disciplinesAbr = new Set();
   getAllPosts().map((post) => {
     const id = post.id;
     const title = post.data.title
     const discipline = post.data.discipline;
     const abr = post.data.abr;
     posts.push({ id, title, discipline, abr });
+    disciplinesAbr.add(abr);
   })
   return {
-    props: { posts: posts }
+    props: { posts: posts, disciplinesAbr: Array.from(disciplinesAbr) }
   }
 }
